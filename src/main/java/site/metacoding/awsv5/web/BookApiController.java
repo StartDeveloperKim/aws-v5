e7cae29369fdc7fdc5cf035ff28d0be88d8b73dc
StartDeveloperKim <ktw550111@naver.com>
2023-05-17 10:23:04 +0900

    배포

--- src/main/java/site/metacoding/awsv5/web/BookApiController.java
+++ src/main/java/site/metacoding/awsv5/web/BookApiController.java
@@ -22,7 +22,7 @@ public class BookApiController {
 
     @GetMapping("/")
     public String home() {
-        return "<h1>aws-v5</h1>";
+        return "<h1>aws-v5 안녕하세요</h1>";
     }
 
     @PostMapping("/api/book")
